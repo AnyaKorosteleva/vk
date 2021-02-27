b5cd82b11d806ae90bc923fdc9daf43f7342f42b
AnyaKorosteleva <annakorosteleva3008@gmail.com>
2021-02-27 22:51:28 +0300

    manager added

---
+++ src/main/java/ru/netology/manager/manager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+public class manager {
+    public class Post {
+        Post [] posts;
+        public Post search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+            return null;
+        }
+    }
+
+    public class Delete {
+        public void delete(int ownerId, int postId) {
+        }
+    }
+}
